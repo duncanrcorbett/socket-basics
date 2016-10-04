aead684be3f2cdd70d29298c950135e742e067ce
Duncan Corbett <duncanrcorbett@gmail.com>
2016-10-04 11:04:12 +0200

    Added customer message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to Socket.io Server');
 });
+
+socket.on('message', function (message) {
+    console.log('New message');
+    console.log(message.text);
+
+});
