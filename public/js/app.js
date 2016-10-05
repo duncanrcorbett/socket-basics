58780bfd6b4b038fa588d3f2f6fd8c7f8018b0c8
Duncan Corbett <duncanrcorbett@gmail.com>
2016-10-05 10:14:32 +0200

    added chat room privacy

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,16 @@ var socket = io();
 
 console.log(name + ' wants to join ' + room);
 
+//update h1 tag
+jQuery('.room-title').text(room);
+
+
 socket.on('connect', function () {
     console.log('Conncted to socket.io server!');
+    socket.emit('joinRoom', {
+        name: name,
+        room: room
+    });
 });
 
 socket.on('message', function (message) {
